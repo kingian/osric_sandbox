f8140f84cf1990ecb0d29786ddc5a2883fd7c532
Ian King <gnik.nai@gmail.com>
2015-10-06 22:53:55 -0600

    Adds a file reader to the project.

---
+++ Assets/Scripts/Services/RPGTableReader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;  
+
+public class RPGTableReader  
+{
+
+	static string[] tableArray;
+
+	void RGBTableReader()
+	{
+	}
+
+
+
+	static public string[] Load(string fileName)
+	{
+		var io = Resources.Load(fileName);
+		tableArray = io.ToString().Split('\n');
+		return tableArray;
+	}
+}
---
+++ Assets/Scripts/fileReaderTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class fileReaderTest : MonoBehaviour {
+
+
+	public string[] lines;
+//	public File fl;
+	// Use this for initialization
+	void Start () 
+	{
+
+
+		lines = RPGTableReader.Load("OSRIC_attribute_table");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
