f856775f7a0d7047e18b3e362519f3f6db9fbe3f
Ian King <gnik.nai@gmail.com>
2015-12-30 10:00:39 -0700

    Got rid of excess classes, i.e. attributes and race.  Added OSRICEngine which will be our big ugly static class.

--- Assets/Scripts/OSRICS_Scripts/OSRICAttributeModel.cs
+++ Assets/Scripts/OSRICS_Scripts/OSRICAttributeModel.cs
@@ -7,29 +7,24 @@ public class OSRICAttributeModel : RPGAttributeModel {
 	public RPGCharacterModel cm;
 
 	//Attributes
-	public OSRICstrength Str;
-	public OSRICdexterity Dex;
-	public OSRICconstitution Con;
-	public OSRICintelligence Int;
-	public OSRICwisdom Wis;
-	public OSRICcharisma Cha;
+	public int Str;
+	public int Dex;
+	public int Con;
+	public int Int;
+	public int Wis;
+	public int Cha;
 
 	public int hitPoints;
 	public int armorClass;
 	public int level;
 	public int experiencePoints;
-	public OSRICCoreRace race;
+
 
 
 
 	void Awake ()
 	{
-		Str = new OSRICstrength(0);
-		Dex = new OSRICdexterity(0);
-		Con = new OSRICconstitution(0);
-		Int = new OSRICintelligence(0);
-		Wis = new OSRICwisdom(0);
-		Cha = new OSRICcharisma(0);
+
 		cm = gameObject.GetComponentInParent<RPGCharacterModel>();
 	}
 
