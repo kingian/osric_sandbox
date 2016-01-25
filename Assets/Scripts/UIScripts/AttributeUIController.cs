4e8b926ca7b62186f50919e83c5b3411ff5f9d60
Ian King <gnik.nai@gmail.com>
2016-01-25 12:25:39 -0700

    Adds control classes for prototype-gui.

---
+++ Assets/Scripts/UIScripts/AttributeUIController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class AttributeUIController : MonoBehaviour {
+
+
+	public Text attributeLabel;
+	public Text attributeValue;
+	public Text attributeSubtext;
+	public string labelString;
+	public string valueString;
+	public string subtextString;
+	public OSRIC_ATTRIBUTES attributeEnum;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if(attributeLabel.text!=attributeEnum.GetDesc())
+			SetAttributeLabel();
+
+	}
+
+	void SetAttributeLabel()
+	{
+		attributeLabel.text = attributeEnum.GetDesc() + ":";
+	}
+
+	void SetAttributeValue(int val)
+	{
+		attributeValue.text = val.ToString();
+	}
+
+	void SetAttributeSubtext(string subt)
+	{
+		attributeSubtext.text = subt;
+	}
+
+
+}
---
+++ Assets/Scripts/UIScripts/AttributeUIGroup.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AttributeUIGroup : MonoBehaviour {
+
+	RPGCharacterModel cmodel;
+	public AttributeUIController[] attributeContollerArray;
+
+	// Use this for initialization
+	void Start () {
+		attributeContollerArray = gameObject.GetComponentsInChildren<AttributeUIController>();
+		OrderAttributeElements();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OrderAttributeElements()
+	{
+		float yOffset = 0f;
+		Vector3 currentPos;
+		GameObject currentGO;
+		foreach(OSRIC_ATTRIBUTES oa in Enum.GetValues(typeof(OSRIC_ATTRIBUTES)))
+		{
+			currentGO = GetController(oa);
+			currentPos =  currentGO.transform.position;
+			currentPos.y += yOffset;
+			currentGO.transform.position = currentPos;
+			yOffset -= 25f;
+		}
+	}
+
+	GameObject GetController(OSRIC_ATTRIBUTES oa)
+	{
+		if(attributeContollerArray.Length<1)
+			return null;
+		foreach(AttributeUIController ac in attributeContollerArray)
+		{
+			if(ac.attributeEnum==oa)
+				return ac.gameObject;
+		}
+		return null;
+	}
+
+	void SetAttributes()
+	{
+		foreach(OSRIC_ATTRIBUTES oa in Enum.GetValues(typeof(OSRIC_ATTRIBUTES)))
+		{
+
+		}
+	}
+}
