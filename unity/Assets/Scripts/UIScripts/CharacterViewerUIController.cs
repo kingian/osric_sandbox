6c8f5dcccad92c8ea6bd0b805e26dcd691d35d7b
Ian King <gnik.nai@gmail.com>
2016-03-18 14:27:03 -0600

    Adds Character Viewer UI Controller and character viewer.

---
+++ unity/Assets/Scripts/UIScripts/CharacterViewerUIController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterViewerUIController : MonoBehaviour {
+
+
+	public AttributeUIGroup attributeGroup;
+	public RPGCharacterModel charModel;
+	public OSRICEngine engine;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
